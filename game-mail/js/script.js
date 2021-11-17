68e511d7dfd3c81596befb18eb9893f4ab03a644
Lore0905 <lore.castelli4@gmail.com>
2021-11-17 16:03:43 +0100

    mail-game done

---
+++ game-mail/js/script.js
@@ -0,0 +1,27 @@
+// GAME MAIL
+
+// fare una lista [array] con un paio di mail
+const mailParticipants = ['gianni.rossi@gmail.com', 'giovanni.bianchi@gmail.com', 'mario.verdi@gmail.com', 'elisabetta.1976gmail.com'];
+
+// chiedere all'utente la mail
+let userMail = prompt('Gentile cliente inserisca quì la sua mail');
+
+// creo la variabile flag
+let verifiedMail = false;
+
+// confrontare se la mail richiesta è presente nella lista iniziale
+
+for ( let i = 0; i < mailParticipants.length; i++ ){
+
+    let totalMail = mailParticipants[i];
+    
+    if (totalMail === userMail){
+        verifiedMail = true;
+    }
+}
+if (verifiedMail){
+    alert('Perfetto!, puoi accedere al file !')
+}
+else{
+    alert('mi dispaice, ma non puoi accedere al file')
+}
